485ffe1ebe00959d40f1dad9ca77932fc74f3cf0
aki <ahsi.aki@gmail.com>
2022-08-27 09:25:09 +0800

    環境建置

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { DatePicker } from 'antd';
+
+export default function Main() {
+  return (
+    <main className="main">
+      <DatePicker/>
+      <h2 className="text-success">TEST TITLE</h2>
+    </main>
+    )
+
+}
