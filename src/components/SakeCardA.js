f366f1a339275b3b971f82ac36937e75ba72bcba
aki <ahsi.aki@gmail.com>
2022-08-27 17:24:50 +0800

    card path

--- src/components/SakeCardA.js
+++ src/components/SakeCardA.js
@@ -2,10 +2,13 @@ import React from 'react'
 import * as bs from 'react-bootstrap'
 
 const SakeCardA = ({sakeImg}) => {
+
+  const imgPath = 'https://raw.githubusercontent.com/aki168/sakeholic/main/public/media/';
+
   return (
     <bs.Card className="col-12 col-md-4 mx-3 my-4 p-0">
       <bs.Card.Header className='p-0' 
-      style={{height:'370px', backgroundImage:`url("../../media/${sakeImg}")`,
+      style={{height:'370px', backgroundImage:`url("${imgPath}${sakeImg}")`,
               backgroundPosition:'center center', backgroundSize:'cover'}} 
       />
       <bs.Card.Body>
