5b83d891cf13bac0a54a280b97547a98b67cb765
DESKTOP-RP4BFD4\AKI <ahsi.aki@gmail.com>
2022-10-02 11:47:36 +0800

    更新首頁回饋卡Tabs元件/ RWD優化版本

---
+++ src/components/ScrollableTabsFeedback.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs, { tabsClasses } from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import FeedbackCard from './FeedbackCard';
+
+export default function ScrollableTabsFeedback({feedbackData}) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  // console.log('data',currentData)
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        maxWidth: { sm: '100%', lg: '100%' },
+      }}
+    >
+      <Tabs
+        textColor="inherit"
+        indicatorColor="none"
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+        aria-label="visible arrows tabs example"
+        sx={{
+          [`& .${tabsClasses.scrollButtons}`]: {
+            '&.Mui-disabled': { opacity: 0.3 },
+          },
+        }}
+      >
+{
+  feedbackData.map((item, i) => (
+    <FeedbackCard key={i} {...item} />
+  ))
+}
+      </Tabs>
+    </Box>
+  );
+}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,24 +3,25 @@ import SakeCardA from "../components/SakeCardA"
 import FeedbackCard from "../components/FeedbackCard"
 import { Title } from '../components/Title'
 import feedbackData from '../data/feedbackData'
-import Dots from "../components/Dots"
+import { Button } from "react-bootstrap"
 import sakeCardDefault from "../data/sakeCardDefault"
+import ScrollableTabsFeedback from "../components/ScrollableTabsFeedback"
 
 export default function Main() {
 
   const data = sakeCardDefault;
 
-  const aosEffectForCard = ['right','left','right','left',]
+  const aosEffectForCard = ['right', 'left', 'right', 'left',]
   //zoom-in-left
 
   return (
     <main>
       <div className="hero">
         <div className="container">
-          {/* <bs.Button variant="outline-dark"
+          {/* <Button variant="outline-dark"
           className="">
             立即搜尋
-          </bs.Button> */}
+          </Button> */}
         </div>
       </div>
 
@@ -28,9 +29,9 @@ export default function Main() {
         <Title cn="日本酒探索" jp="お気に入りが見つかる" />
         <div className="row justify-content-md-around">
           {data.map((item, i) => (
-          <div data-aos={`zoom-in-${aosEffectForCard[i]}`} className='col-11 col-md-5 col-lg-4 mx-3 my-4 p-0' >
-            <SakeCardA  key={i} {...item} />
-          </div>)
+            <div data-aos={`zoom-in-${aosEffectForCard[i]}`} className='col-11 col-md-5 col-lg-4 mx-3 my-4 p-0' >
+              <SakeCardA key={i} {...item} />
+            </div>)
           )}
         </div>
       </section>
@@ -38,14 +39,18 @@ export default function Main() {
       <section className="px-5 py-3 overflow-auto">
         <Title cn="網友回饋" jp="みんなの声" />
         <div
-          className="row mx-0 flex-md-nowrap 
-          mb-1
-        ">
-          {feedbackData.map((item, i) => (
-            <FeedbackCard key={i} {...item} />
-          ))}
+          className="row mx-0 flex-md-nowrap mb-1 d-md-none">
+        {/* mobile */}
+          {
+            feedbackData.map((item, i) => (
+              <FeedbackCard key={i} {...item} />
+            ))
+          }
         </div>
-        {/* <Dots /> */}
+        {/* PC */}
+        <div className="d-none d-md-block mb-2">
+          <ScrollableTabsFeedback feedbackData={feedbackData} />
+        </div>
       </section>
     </main>
   )
